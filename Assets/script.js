3ddecf2330d73331758bf5c2b06c98fed83e10d8
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-06-15 22:07:05 -0700

    This is a version, where the buttons and search function work

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,105 @@
 var apiKEY = "cbe8fc6d3eba09369c7445d7ce20d535";
-var cityName = "Seattle";
+var unitsMeasurement = "imperial";
+var cityTemp ="";
+var cityForecast = "";
+var cityName = "";
+var replace1 = document.querySelector(".temperature");
+var replace2 = document.querySelector(".precipitation");
+var replace3 = document.querySelector(".city");
+var replace4 = document.querySelector(".wind");
+var replace5 = document.querySelector(".uv")
+var listenButton = document.querySelector(".search-button");
+var Austin = document.querySelector("#Austin");
+var Chicago = document.querySelector("#Chicago");
+var NewYork = document.querySelector("#NewYork");
+var Orlando = document.querySelector("#Orlando");
+var SanFrancisco = document.querySelector("#SanFrancisco");
+var Seattle = document.querySelector("#Seattle");
+var Denver = document.querySelector("#Denver");
+var temperature = "";
+
+
+ 
+//https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=cbe8fc6d3eba09369c7445d7ce20d535&unit=metric
 
 
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-
-function cityWeather() {
-     fetch(queryURL);
-    console.log();
+function updateTable () {
+  event.preventDefault();
+  var text = $('.search').val();
+  localStorage.setItem('query', text);
+  cityName = localStorage.getItem('query');
+  cityName = text;
+  console.log(cityName);
+  requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
+  getdata();
 }
 
-cityWeather();
+function austinData () {
+  cityName = "Austin";
+  getdata();
+}
+
+function chicagoData () {
+  cityName = "Chicago";
+  getdata();
+}
+
+function newyorkData() {
+  cityName = "New York";
+  getdata();
+}
+
+
+function orlandoData() {
+  cityName = "Orlando";
+  getdata();
+}
+
+function sfData() {
+  cityName ="San Francisco";
+  getdata();
+}
+
+function seattleData() {
+  cityName = "Seattle";
+  getdata();
+}
+
+
+function getdata () {
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
+  fetch(requestUrl)
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+     
+     console.log(data);
+     console.log("This is the city's temperature " + data.main.temp );
+     console.log("This is " + cityName + "forecast" + data.clouds.all)
+     cityTemp =  data.main.temp;
+     cityForecast = data.clouds.all;
+     console.log("This is a check to see if value is passed to global " + cityTemp)
+     replace1.innerHTML = "Temp: " + cityTemp;
+     replace2.innerHTML = "Humidity: " + cityForecast;
+     replace3.innerHTML = data.name;
+     replace4.innerHTML = "Wind: " + data.wind.speed + " MPH";
+     replace5.innerHTML = "UV: " + data.clouds.all + "%";
+ 
+     return cityTemp;
+   });
+ }
+
+listenButton.addEventListener('click', updateTable);
+Austin.addEventListener('click', austinData);
+Chicago.addEventListener('click', chicagoData);
+NewYork.addEventListener('click', newyorkData);
+Orlando.addEventListener('click', orlandoData);
+SanFrancisco.addEventListener('click',sfData);
+Seattle.addEventListener('click',seattleData);
+
+getdata();
+
+
+console.log("This is a check to see if value is passed to global " + cityTemp)
