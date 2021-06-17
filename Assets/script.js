c59775bc991aaa70a2ea501150be52efb08295dd
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-06-16 23:17:51 -0700

    This adds forecast functionality via API, and also allows a gradient to determine the UV

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,7 @@ var unitsMeasurement = "imperial";
 var cityTemp ="";
 var cityForecast = "";
 var cityName = "";
+var cityNamePrevious = "";
 var replace1 = document.querySelector(".temperature");
 var replace2 = document.querySelector(".precipitation");
 var replace3 = document.querySelector(".city");
@@ -16,10 +17,15 @@ var Orlando = document.querySelector("#Orlando");
 var SanFrancisco = document.querySelector("#SanFrancisco");
 var Seattle = document.querySelector("#Seattle");
 var Denver = document.querySelector("#Denver");
+var Atlanta = document.querySelector("#Atlanta");
+var onlyMonthDayYear = moment().format("MM/DD/YYYY");
+
+
 var temperature = "";
 
 
- 
+//https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+//https://pro.openweathermap.org/data/2.5/forecast/hourly?q=Seattle&appid=cbe8fc6d3eba09369c7445d7ce20d535
 //https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=cbe8fc6d3eba09369c7445d7ce20d535&unit=metric
 
 
@@ -36,34 +42,60 @@ function updateTable () {
 }
 
 function austinData () {
+  cityNamePrevious = cityName;
   cityName = "Austin";
   getdata();
+  colorChange();
 }
 
 function chicagoData () {
+  cityNamePrevious = cityName;
   cityName = "Chicago";
   getdata();
+  colorChange();
 }
 
 function newyorkData() {
+  cityNamePrevious = cityName;
   cityName = "New York";
   getdata();
+  colorChange();
 }
 
 
 function orlandoData() {
+  cityNamePrevious = cityName;
   cityName = "Orlando";
   getdata();
+  colorChange();
 }
 
 function sfData() {
+  cityNamePrevious = cityName;
   cityName ="San Francisco";
   getdata();
+  colorChange();
 }
 
 function seattleData() {
+  cityNamePrevious = cityName;
   cityName = "Seattle";
   getdata();
+  colorChange();
+}
+
+function denverData() {
+  cityNamePrevious = cityName;
+  cityName = "Denver"
+  getdata();
+  colorChange();
+}
+
+function atlantaData() {
+  cityNamePrevious = cityName;
+  cityName = "Atlanta"
+  getdata();
+  colorChange();
 }
 
 
@@ -77,19 +109,81 @@ function getdata () {
      
      console.log(data);
      console.log("This is the city's temperature " + data.main.temp );
-     console.log("This is " + cityName + "forecast" + data.clouds.all)
+     console.log("This is " + cityName + "forecast" + data.clouds.all);
      cityTemp =  data.main.temp;
      cityForecast = data.clouds.all;
-     console.log("This is a check to see if value is passed to global " + cityTemp)
+     console.log("This is a check to see if value is passed to global " + cityTemp);
      replace1.innerHTML = "Temp: " + cityTemp;
      replace2.innerHTML = "Humidity: " + cityForecast;
-     replace3.innerHTML = data.name;
+    
+     replace3.innerHTML = data.name + " " + onlyMonthDayYear;
      replace4.innerHTML = "Wind: " + data.wind.speed + " MPH";
      replace5.innerHTML = "UV: " + data.clouds.all + "%";
- 
-     return cityTemp;
+     localStorage.setItem('previous-search',cityNamePrevious);
+     var retrieve = localStorage.getItem('previous-search');
+     var pastResult = document.querySelector("#past-search");
+     pastResult.innerHTML = retrieve;
+    colorChange();
+     return cityName;
    });
+   
  }
+
+
+ function colorChange () {
+   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
+   fetch(requestUrl)
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+    var gradient = data.clouds.all;
+    console.log(gradient);
+                if (gradient <= 20){
+                    $('.uv').css("background-color" , "red");
+                } else if (gradient > 20 && gradient <= 40) {
+                    $('.uv').css("background-color" , "orange");
+                } else if (gradient > 40 && gradient <= 60 ) {
+                    $('.uv').css("background-color" , "yellow");
+                } else if (gradient > 60 && gradient <= 80) {
+                    $('.uv').css("background-color" , "green");
+                } else if (gradient > 80 && gradient <= 100) {
+                    $('.uv').css("background0color" , "lime");
+                }
+
+
+                getFuture();
+    return;
+ 
+   });
+   
+  }
+
+
+function getFuture() {
+  console.log("getFuture function has initiated");
+  var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
+  fetch(requestUrl)
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+     $("#day-one-time").text(data.list[0].dt_txt);
+     $("#day-two-time").text(data.list[8].dt_txt);
+     $("#day-three-time").text(data.list[16].dt_txt);
+     $("#day-four-time").text(data.list[24].dt_txt);
+     $("#day-five-time").text(data.list[8].dt_txt);
+     console.log(data.list[0].dt_txt);
+     return;
+   });
+   
+  
+}
+
+
+
+
+ 
 
 listenButton.addEventListener('click', updateTable);
 Austin.addEventListener('click', austinData);
@@ -98,8 +192,11 @@ NewYork.addEventListener('click', newyorkData);
 Orlando.addEventListener('click', orlandoData);
 SanFrancisco.addEventListener('click',sfData);
 Seattle.addEventListener('click',seattleData);
+Denver.addEventListener('click',denverData);
+Atlanta.addEventListener('click',atlantaData);
 
 getdata();
+getFuture();
 
 
-console.log("This is a check to see if value is passed to global " + cityTemp)
+console.log("This is a check to see if value is passed to global " + cityTemp);
