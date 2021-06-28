3d0281ae35fcc6691fa74665f3ea2fb800a9d4a6
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-06-27 18:35:09 -0700

    This is a commit with a working weather icons

--- Assets/script.js
+++ Assets/script.js
@@ -19,6 +19,7 @@ var Seattle = document.querySelector("#Seattle");
 var Denver = document.querySelector("#Denver");
 var Atlanta = document.querySelector("#Atlanta");
 var onlyMonthDayYear = moment().format("MM/DD/YYYY");
+var previousCity = document.querySelector("#previous-data");
 
 
 var temperature = "";
@@ -33,67 +34,91 @@ var temperature = "";
 function updateTable () {
   event.preventDefault();
   var text = $('.search').val();
-  cityNamePrevious = text;
-  localStorage.setItem('query', text);
-  cityName = localStorage.getItem('query');
+  
+  localStorage.setItem('previous-search',text);
+  console.log("updateTable has a localstorage vauloe of " + localStorage.getItem('previous-search'));
   cityName = text;
-  console.log(cityName);
-  requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
+  $("#previous-data").text(localStorage.getItem('previous-search'));
+  //requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
   getdata();
 }
 
 function austinData () {
-  cityNamePrevious = cityName;
+  console.log("austinData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("austinData has the localstorage value of" + localStorage.getItem('previous-search'));
+  
   cityName = "Austin";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function chicagoData () {
-  cityNamePrevious = cityName;
+  console.log("chicagoData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("chicagoData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "Chicago";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function newyorkData() {
-  cityNamePrevious = cityName;
+  console.log("newYorkData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("newyorkData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "New York";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 
 function orlandoData() {
-  cityNamePrevious = cityName;
+  console.log("orlandoData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("orlandoData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "Orlando";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function sfData() {
-  cityNamePrevious = cityName;
+  console.log("sfData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("sfData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName ="San Francisco";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function seattleData() {
-  cityNamePrevious = cityName;
+  console.log("seattleData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("seattleData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "Seattle";
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function denverData() {
-  cityNamePrevious = cityName;
+  console.log("denverData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("denverData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "Denver"
+  $("#previous-data").text(localStorage.getItem('previous-search'));
   getdata();
   colorChange();
 }
 
 function atlantaData() {
-  cityNamePrevious = cityName;
+  console.log("atlantaData has started");
+  localStorage.setItem('previous-search',cityName);
+  console.log("atlantaData has the localstorage value of" + localStorage.getItem('previous-search'));
   cityName = "Atlanta"
   getdata();
   colorChange();
@@ -101,6 +126,8 @@ function atlantaData() {
 
 
 function getdata () {
+  console.log(localStorage.getItem('previous-search'));
+  console.log(cityName);
   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKEY + "&units=" + unitsMeasurement;
   fetch(requestUrl)
    .then(function (response) {
@@ -111,6 +138,9 @@ function getdata () {
      console.log(data);
      console.log("This is the city's temperature " + data.main.temp );
      console.log("This is " + cityName + "forecast" + data.clouds.all);
+     var iconCode = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+     $("#icon").attr('src', iconCode);
+     cityNamePrevious = cityName;
      cityTemp =  data.main.temp;
      cityForecast = data.clouds.all;
      console.log("This is a check to see if value is passed to global " + cityTemp);
@@ -120,12 +150,11 @@ function getdata () {
      replace3.innerHTML = data.name + " " + onlyMonthDayYear;
      replace4.innerHTML = "Wind: " + data.wind.speed + " MPH";
      replace5.innerHTML = "UV: " + data.clouds.all + "%";
-     localStorage.setItem('previous-search',cityNamePrevious);
-     var retrieve = localStorage.getItem('previous-search');
-     var pastResult = document.querySelector("#past-search");
-     pastResult.innerHTML = retrieve;
+     
+    
+     
     colorChange();
-     return cityName;
+     return;
    });
    
  }
@@ -169,6 +198,22 @@ function getFuture() {
      return response.json();
    })
    .then(function (data) {
+    var iconCode1 = "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+     $("#icon1").attr('src', iconCode1);
+     var iconCode2 = "https://openweathermap.org/img/w/" + data.list[8].weather[0].icon + ".png";
+     $("#icon2").attr('src', iconCode2);
+     var iconCode3 = "https://openweathermap.org/img/w/" + data.list[16].weather[0].icon + ".png";
+     $("#icon3").attr('src', iconCode3);
+     var iconCode4 = "https://openweathermap.org/img/w/" + data.list[24].weather[0].icon + ".png";
+     $("#icon4").attr('src', iconCode4);
+     var iconCode5 = "https://openweathermap.org/img/w/" + data.list[32].weather[0].icon + ".png";
+     $("#icon5").attr('src', iconCode5);
+
+    $("#city-name1").text(cityName);
+    $("#city-name2").text(cityName);
+    $("#city-name3").text(cityName);
+    $("#city-name4").text(cityName);
+    $("#city-name5").text(cityName);
      $("#day-one-time").text(data.list[0].dt_txt);
      $("#day-two-time").text(data.list[8].dt_txt);
      $("#day-three-time").text(data.list[16].dt_txt);
@@ -204,6 +249,17 @@ function getFuture() {
   
 }
 
+function previousData() {
+  console.log("The previousData function is running");
+  console.log("The previousData function has" + localStorage.getItem('previous-search') + "for the value of cityNamePrevious");
+ 
+ 
+  
+  $("#previous-data").text(localStorage.getItem('previous-search'));
+  cityName = localStorage.getItem('previous-search');
+  
+  getdata();
+}
 
 
 
@@ -218,9 +274,12 @@ SanFrancisco.addEventListener('click',sfData);
 Seattle.addEventListener('click',seattleData);
 Denver.addEventListener('click',denverData);
 Atlanta.addEventListener('click',atlantaData);
+previousCity.addEventListener('click', previousData);
+
 
 getdata();
 getFuture();
+
 
 
 console.log("This is a check to see if value is passed to global " + cityTemp);
